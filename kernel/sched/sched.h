3a011ae5b6d7442c93a274340b49246a3ff862a2
Peter Zijlstra <peterz@infradead.org>
2019-05-29 20:36:42 +0000

    UPSTREAM: sched/fair: Expose newidle_balance()
    (cherry picked from commit 5ba553eff0c3a)
    
    For pick_next_task_fair() it is the newidle balance that requires
    dropping the rq->lock; provided we do put_prev_task() early, we can
    also detect the condition for doing newidle early.
    
    BUG=b:152605392
    TEST=run power_VideoCall test
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Aaron Lu <aaron.lwe@gmail.com>
    Cc: Valentin Schneider <valentin.schneider@arm.com>
    Cc: mingo@kernel.org
    Cc: Phil Auld <pauld@redhat.com>
    Cc: Julien Desfossez <jdesfossez@digitalocean.com>
    Cc: Nishanth Aravamudan <naravamudan@digitalocean.com>
    Link: https://lkml.kernel.org/r/9e3eb1859b946f03d7e500453a885725b68957ba.1559129225.git.vpillai@digitalocean.com
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I7668fc1a0f04219846e1b78d899aa6d06780aecc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2186287
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1582,10 +1582,14 @@ static inline void unregister_sched_domain_sysctl(void)
 }
 #endif
 
+extern int newidle_balance(struct rq *this_rq, struct rq_flags *rf);
+
 #else
 
 static inline void flush_smp_call_function_from_idle(void) { }
 static inline void sched_ttwu_pending(void) { }
+
+static inline int newidle_balance(struct rq *this_rq, struct rq_flags *rf) { return 0; }
 
 #endif /* CONFIG_SMP */
 
