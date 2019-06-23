179d5d0912bc7a6de597d1a4330e70c50f48d320
Yaroslav Furman <yaro330@gmail.com>
2019-06-23 21:40:43 +0300

    kernel: silence suspends/resume logging
    This is so fucking annoying.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1350,7 +1350,7 @@ int freeze_secondary_cpus(int primary)
 	 */
 	cpumask_clear(frozen_cpus);
 
-	pr_info("Disabling non-boot CPUs ...\n");
+	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == primary)
 			continue;
