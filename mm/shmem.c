4e79a566f856a5e6858fff3eb894f3657c6752e5
Al Viro <viro@zeniv.linux.org.uk>
2019-09-08 19:11:46 -0400

    UPSTREAM: shmem_parse_options(): don't bother with mpol in separate variable
    just use ctx->mpol (note that callers always set ctx->mpol to NULL when
    calling that).
    
    Change-Id: Ib8f9a31696115c910ebf0b04248f149ab302553d
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Kneba <kneba@users.noreply.github.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -3374,7 +3374,6 @@ static const struct export_operations shmem_export_ops = {
 static int shmem_parse_options(char *options, struct shmem_options *ctx)
 {
 	char *this_char, *value, *rest;
-	struct mempolicy *mpol = NULL;
 	uid_t uid;
 	gid_t gid;
 
@@ -3460,9 +3459,9 @@ static int shmem_parse_options(char *options, struct shmem_options *ctx)
 #endif
 #ifdef CONFIG_NUMA
 		} else if (!strcmp(this_char,"mpol")) {
-			mpol_put(mpol);
-			mpol = NULL;
-			if (mpol_parse_str(value, &mpol))
+			mpol_put(ctx->mpol);
+			ctx->mpol = NULL;
+			if (mpol_parse_str(value, &ctx->mpol))
 				goto bad_val;
 #endif
 		} else {
@@ -3470,14 +3469,14 @@ static int shmem_parse_options(char *options, struct shmem_options *ctx)
 			goto error;
 		}
 	}
-	ctx->mpol = mpol;
 	return 0;
 
 bad_val:
 	pr_err("tmpfs: Bad value '%s' for mount option '%s'\n",
 	       value, this_char);
 error:
-	mpol_put(mpol);
+	mpol_put(ctx->mpol);
+	ctx->mpol = NULL;
 	return 1;
 
 }
