a565efdb7e4e3d2eb5d203a1e2408e11d6cd9c7c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-02-09 16:24:29 -0800

    simple_lmk: Report mm as freed as soon as exit_mmap() finishes
    exit_mmap() is responsible for freeing the vast majority of an mm's
    memory; in order to unblock Simple LMK faster, report an mm as freed as
    soon as exit_mmap() finishes.
    
    Change-Id: I6ee0dd1a5dbf05de7188555204a2d6a36e769345
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1038,6 +1038,7 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
+	simple_lmk_mm_freed(mm);
 	mm_put_huge_zero_page(mm);
 	set_mm_exe_file(mm, NULL);
 	if (!list_empty(&mm->mmlist)) {
@@ -1048,7 +1049,6 @@ static inline void __mmput(struct mm_struct *mm)
 	if (mm->binfmt)
 		module_put(mm->binfmt->module);
 	lru_gen_del_mm(mm);
-	simple_lmk_mm_freed(mm);
 	mmdrop(mm);
 }
 
