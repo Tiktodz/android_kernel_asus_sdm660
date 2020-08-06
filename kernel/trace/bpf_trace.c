1ac23b1935b62f582f32f3aadb2028b7c60ba59b
Stanislav Fomichev <sdf@google.com>
2020-08-06 11:26:12 -0700

    BACKPORT: bpf: Remove inline from bpf_do_trace_printk
    I get the following error during compilation on my side:
    kernel/trace/bpf_trace.c: In function 'bpf_do_trace_printk':
    kernel/trace/bpf_trace.c:386:34: error: function 'bpf_do_trace_printk' can never be inlined because it uses variable argument lists
     static inline __printf(1, 0) int bpf_do_trace_printk(const char *fmt, ...)
                                      ^
    Backport: Also correct the location of this function.
    
    Fixes: ac5a72ea5c89 ("bpf: Use dedicated bpf_trace_printk event instead of trace_printk()")
    Change-Id: Ib9cad45627704db75d4d718ddecee6029d9dfdf2
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20200806182612.1390883-1-sdf@google.com

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -384,6 +384,30 @@ static void bpf_trace_copy_string(char *buf, void *unsafe_ptr, char fmt_ptype,
 	}
 }
 
+static DEFINE_RAW_SPINLOCK(trace_printk_lock);
+
+#define BPF_TRACE_PRINTK_SIZE   1024
+
+static __printf(1, 0) int bpf_do_trace_printk(const char *fmt, ...)
+{
+	static char buf[BPF_TRACE_PRINTK_SIZE];
+	unsigned long flags;
+	va_list ap;
+	int ret;
+
+	raw_spin_lock_irqsave(&trace_printk_lock, flags);
+	va_start(ap, fmt);
+	ret = vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	/* vsnprintf() will not append null for zero-length strings */
+	if (ret == 0)
+		buf[0] = '\0';
+	trace_bpf_trace_printk(buf);
+	raw_spin_unlock_irqrestore(&trace_printk_lock, flags);
+
+	return ret;
+}
+
 /*
  * Only limited trace_printk() conversion specifiers allowed:
  * %d %i %u %x %ld %li %lu %lx %lld %lli %llu %llx %p %pks %pus %s
