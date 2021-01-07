e3a269c8336b946393c8b08b8f6ab1d88aaf300a
Vincent Guittot <vincent.guittot@linaro.org>
2021-01-07 11:33:24 +0100

    UPSTREAM: sched/fair: Don't set LBF_ALL_PINNED unnecessarily
    Setting LBF_ALL_PINNED during active load balance is only valid when there
    is only 1 running task on the rq otherwise this ends up increasing the
    balance interval whereas other tasks could migrate after the next interval
    once they become cache-cold as an example.
    
    LBF_ALL_PINNED flag is now always set it by default. It is then cleared
    when we find one task that can be pulled when calling detach_tasks() or
    during active migration.
    
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Link: https://lkml.kernel.org/r/20210107103325.30851-3-vincent.guittot@linaro.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Santhosh <san8754513543@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11343,6 +11343,9 @@ no_move:
 			/* Record that we found at least one task that could run on this_cpu */
 			env.flags &= ~LBF_ALL_PINNED;
 
+			/* Record that we found at least one task that could run on this_cpu */
+			env.flags &= ~LBF_ALL_PINNED;
+
 			/*
 			 * ->active_balance synchronizes accesses to
 			 * ->active_balance_work.  Once set, it's cleared
