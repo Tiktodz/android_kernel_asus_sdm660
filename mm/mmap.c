8af9713ee6fbbc37785635717c5efb6dcc682a43
darkhz <kmachanwenw@gmail.com>
2021-02-04 18:52:12 +0300

    mm: Do not reserve memory for admins/users
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- mm/mmap.c
+++ mm/mmap.c
@@ -3816,11 +3816,6 @@ void __init mmap_init(void)
  */
 static int init_user_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_zone_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_user_reserve_kbytes = min(free_kbytes / 32, 1UL << 17);
 	return 0;
 }
 subsys_initcall(init_user_reserve);
@@ -3837,11 +3832,6 @@ subsys_initcall(init_user_reserve);
  */
 static int init_admin_reserve(void)
 {
-	unsigned long free_kbytes;
-
-	free_kbytes = global_zone_page_state(NR_FREE_PAGES) << (PAGE_SHIFT - 10);
-
-	sysctl_admin_reserve_kbytes = min(free_kbytes / 32, 1UL << 13);
 	return 0;
 }
 subsys_initcall(init_admin_reserve);
