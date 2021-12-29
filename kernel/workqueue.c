924f60e04cb89c462d3196c79943367e31781531
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-12-30 04:54:03 +0600

    Revert "workqueue: respect isolated cpus when queueing an unbound work"
    We don't use isolcpus parameter at the kernel's cmdline.
    Therefore, we don't need this.
    
    This reverts commit c98a9805096460567404799a7bd3149826affde7.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -47,7 +47,6 @@
 #include <linux/nodemask.h>
 #include <linux/moduleparam.h>
 #include <linux/uaccess.h>
-#include <linux/sched/isolation.h>
 #include <linux/nmi.h>
 #include <linux/bug.h>
 #include <linux/delay.h>
@@ -5173,10 +5172,6 @@ int workqueue_set_unbound_cpumask(cpumask_var_t cpumask)
 	int ret = -EINVAL;
 	cpumask_var_t saved_cpumask;
 
-	/*
-	 * Not excluding isolated cpus on purpose.
-	 * If the user wishes to include them, we allow that.
-	 */
 	cpumask_and(cpumask, cpumask, cpu_possible_mask);
 	if (!cpumask_empty(cpumask)) {
 		apply_wqattrs_lock();
@@ -5787,13 +5782,12 @@ static void __init wq_numa_init(void)
 int __init workqueue_init_early(void)
 {
 	int std_nice[NR_STD_WORKER_POOLS] = { 0, HIGHPRI_NICE_LEVEL };
-	int hk_flags = HK_FLAG_DOMAIN | HK_FLAG_WQ;
 	int i, cpu;
 
 	WARN_ON(__alignof__(struct pool_workqueue) < __alignof__(long long));
 
 	BUG_ON(!alloc_cpumask_var(&wq_unbound_cpumask, GFP_KERNEL));
-	cpumask_copy(wq_unbound_cpumask, cpu_lp_mask, housekeeping_cpumask(hk_flags));
+	cpumask_copy(wq_unbound_cpumask, cpu_lp_mask);
 
 	pwq_cache = KMEM_CACHE(pool_workqueue, SLAB_PANIC);
 
