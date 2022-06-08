90c7601c1d0078aaed91320c5c29dc565be5d327
RyuujiX <saputradenny712@gmail.com>
2022-06-08 22:22:52 +0700

    Revert "kernel/power: faster deepsleep"
    This reverts commit c423155df592433043762d096645eb32942bca5d.
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -27,7 +27,7 @@
  * Timeout for stopping processes
  */
 unsigned int __read_mostly freeze_timeout_msecs =
-	IS_ENABLED(CONFIG_ANDROID) ? MSEC_PER_SEC : 2 * MSEC_PER_SEC;
+	IS_ENABLED(CONFIG_ANDROID) ? MSEC_PER_SEC : 20 * MSEC_PER_SEC;
 
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
