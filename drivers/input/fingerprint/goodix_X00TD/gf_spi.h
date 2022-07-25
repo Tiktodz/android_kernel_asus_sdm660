0524c8f542d4184f62fab050e9e8daf85585ce0d
Vivekachooz <vivekachooz@gmail.com>
2022-07-25 18:53:24 +0700

    drivers: input: fingerprint: Decommonize goodixfp from commonfp
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/input/fingerprint/goodix_X00TD/gf_spi.h
+++ drivers/input/fingerprint/goodix_X00TD/gf_spi.h
@@ -129,16 +129,16 @@ struct gf_dev {
 	unsigned users;
 	signed irq_gpio;
 	signed reset_gpio;
-	signed pwr_gpio;
+	signed vdd_gpio;
 	int irq;
-	int irq_enabled;
-	int clk_enabled;
+	bool irq_enabled;
+	bool clk_enabled;
 #ifdef GF_FASYNC
 	struct fasync_struct *async;
 #endif
 	struct notifier_block notifier;
-	char device_available;
-	char fb_black;
+	bool device_available;
+	bool fb_black;
 };
 
 int gf_parse_dts(struct gf_dev* gf_dev);
--- drivers/input/fingerprint/goodix_X00TD/platform.c
+++ drivers/input/fingerprint/goodix_X00TD/platform.c
@@ -12,7 +12,6 @@
 #include <linux/err.h>
 
 #include "gf_spi.h"
-#include "../common_X00TD/fingerprint_common.h"
 
 #if defined(USE_SPI_BUS)
 #include <linux/spi/spi.h>
@@ -21,40 +20,52 @@
 #include <linux/platform_device.h>
 #endif
 
-#ifndef USE_COMMON_FP
 int gf_parse_dts(struct gf_dev* gf_dev)
 {
 	int rc = 0;
 
+	/*get vdd resource*/
+    gf_dev->vdd_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"goodix,gpio_vdd",0);
+    if(!gpio_is_valid(gf_dev->vdd_gpio)) {
+        pr_info("VDD GPIO is invalid.\n");
+        return -1;
+    }
+    rc = gpio_request(gf_dev->vdd_gpio, "goodix_vdd");
+    if(rc) {
+        dev_err(&gf_dev->spi->dev, "Failed to request PWR GPIO. rc = %d\n", rc);
+        return -1;
+    }
+
 	/*get reset resource*/
-	gf_dev->reset_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"fp-gpio-reset",0);
+	gf_dev->reset_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"goodix,reset_gpio",0);
 	if(!gpio_is_valid(gf_dev->reset_gpio)) {
 		pr_info("RESET GPIO is invalid.\n");
 		return -1;
 	}
-
 	rc = gpio_request(gf_dev->reset_gpio, "goodix_reset");
 	if(rc) {
 		dev_err(&gf_dev->spi->dev, "Failed to request RESET GPIO. rc = %d\n", rc);
 		return -1;
 	}
-
 	gpio_direction_output(gf_dev->reset_gpio, 1);
 
 	/*get irq resourece*/
-	gf_dev->irq_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"fp-gpio-irq",0);
+	gf_dev->irq_gpio = of_get_named_gpio(gf_dev->spi->dev.of_node,"goodix,irq_gpio",0);
 	pr_info("gf::irq_gpio:%d\n", gf_dev->irq_gpio);
 	if(!gpio_is_valid(gf_dev->irq_gpio)) {
 		pr_info("IRQ GPIO is invalid.\n");
 		return -1;
 	}
-
 	rc = gpio_request(gf_dev->irq_gpio, "goodix_irq");
 	if(rc) {
 		dev_err(&gf_dev->spi->dev, "Failed to request IRQ GPIO. rc = %d\n", rc);
 		return -1;
 	}
 	gpio_direction_input(gf_dev->irq_gpio);
+
+    gpio_direction_output(gf_dev->vdd_gpio, 1);
+
+	msleep(10);
 
 	return 0;
 }
@@ -72,40 +83,37 @@ void gf_cleanup(struct gf_dev	* gf_dev)
 		gpio_free(gf_dev->reset_gpio);
 		pr_info("remove reset_gpio success\n");
 	}
+	if (gpio_is_valid(gf_dev->vdd_gpio))
+    {
+        gpio_free(gf_dev->vdd_gpio);
+        pr_info("remove vdd_gpio success\n");
+    }
 }
-#endif
 
 int gf_power_on(struct gf_dev* gf_dev)
 {
-#ifndef USE_COMMON_FP
-	int rc = 0;
+    int rc = 0;
+    if (gpio_is_valid(gf_dev->vdd_gpio)) {
+        gpio_set_value(gf_dev->vdd_gpio, 1);
+    }
+    msleep(10);
+    pr_info("---- power on ok ----\n");
 
-	msleep(10);
-	pr_info("---- power on ok ----\n");
-
-	return rc;
-#else
-	return commonfp_power_on();
-#endif
+    return rc;
 }
 
 int gf_power_off(struct gf_dev* gf_dev)
 {
-#ifndef USE_COMMON_FP
-	int rc = 0;
-
-	msleep(10);
-	pr_info("---- power off ok ----\n");
-
-	return rc;
-#else
-	return commonfp_power_off();
-#endif
+    int rc = 0;			
+    if (gpio_is_valid(gf_dev->vdd_gpio)) {
+        gpio_set_value(gf_dev->vdd_gpio, 1);
+    }
+    pr_info("---- power off ----\n");
+    return rc;
 }
 
 int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms)
 {
-#ifndef USE_COMMON_FP
 	if(gf_dev == NULL) {
 		pr_info("Input buff is NULL.\n");
 		return -1;
@@ -116,12 +124,8 @@ int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms)
 	gpio_set_value(gf_dev->reset_gpio, 1);
 	mdelay(delay_ms);
 	return 0;
-#endif
-
-	return commonfp_hw_reset(3);
 }
 
-#ifndef USE_COMMON_FP
 int gf_irq_num(struct gf_dev *gf_dev)
 {
 	if(gf_dev == NULL) {
@@ -131,5 +135,3 @@ int gf_irq_num(struct gf_dev *gf_dev)
 		return gpio_to_irq(gf_dev->irq_gpio);
 	}
 }
-#endif
-
