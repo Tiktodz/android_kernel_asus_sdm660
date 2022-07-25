fbd99328a03e5d20f991cb427f17c0e7135c9a65
anomalchik <anomalchik@users.noreply.github.com>
2022-07-25 18:15:37 +0700

    input: fingerprint: goodix_X00TD: Rename input event
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/input/fingerprint/goodix_X00TD/gf_spi.c
+++ drivers/input/fingerprint/goodix_X00TD/gf_spi.c
@@ -61,7 +61,7 @@
 #define GF_SPIDEV_NAME     "goodix,fingerprint"
 /*device name after register in charater*/
 #define GF_DEV_NAME            "goodix_fp"
-#define	GF_INPUT_NAME	    "qwerty"	/*"goodix_fp" */
+#define	GF_INPUT_NAME	    "gxfp_input"	/*"goodix_fp" */
 
 #define	CHRD_DRIVER_NAME	"goodix_fp_spi"
 #define	CLASS_NAME		    "goodix_fp"
