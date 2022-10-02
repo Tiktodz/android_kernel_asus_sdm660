83c177b7071b0bb3d7ed3391e1c94e9ea9b5dac7
Albert I <krascgq@outlook.co.id>
2022-10-02 16:54:56 +0700

    ASUS: NVT_Touch: Debloat the driver
    * disable driver debug
    * dont build the testing driver
    
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/input/touchscreen/NVT_Touch_ASUS/nt36xxx.h
+++ drivers/input/touchscreen/NVT_Touch_ASUS/nt36xxx.h
@@ -31,7 +31,7 @@
 
 #include "nt36xxx_mem_map.h"
 
-#define NVT_DEBUG 1
+#define NVT_DEBUG 0
 
 //---GPIO number---
 #define NVTTOUCH_RST_PIN 980
@@ -85,7 +85,7 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 //---Customerized func.---
 #define NVT_TOUCH_PROC 1
 #define NVT_TOUCH_EXT_PROC 1
-#define NVT_TOUCH_MP 1
+#define NVT_TOUCH_MP 0
 #define MT_PROTOCOL_B 1
 #define WAKEUP_GESTURE 1
 #if WAKEUP_GESTURE
