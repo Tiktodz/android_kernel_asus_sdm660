a55d84262af415b3bba01145f32a805009411cc5
Divyanshu-Modi <divyan.m05@gmail.com>
2022-10-02 20:49:17 +0700

    ASUS: NVT_Touch: Use a RR thread for work processing
    based on: tytydraco/android_kernel_xiaomi_jasmine@b0219a4
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- drivers/input/touchscreen/NVT_Touch_ASUS/nt36xxx.h
+++ drivers/input/touchscreen/NVT_Touch_ASUS/nt36xxx.h
@@ -24,6 +24,7 @@
 #include <linux/input.h>
 #include <linux/uaccess.h>
 #include <linux/regulator/consumer.h>
+#include <uapi/linux/sched/types.h>
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #include <linux/earlysuspend.h>
