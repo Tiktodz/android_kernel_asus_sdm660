c518d84fe1d809a847e316e976a8e81ec3499370
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-01-15 20:54:53 +0600

    devfreq: Sync and adapt to floral's devfreq boost
    This syncs devfreq boost to Sultan Alsawaf's Floral kernel repository [1]
    and adapts changes to our existing driver.
    
    [1]:
    (a) https://github.com/kerneltoast/android_kernel_google_floral/commit/f3860ae3cfd89af6719d59aa92d9ff73f96d9251
    (b) https://github.com/kerneltoast/android_kernel_google_floral/commit/8db02ebfa4e15e287beb8d11f6b439907ad83819
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4740,6 +4740,9 @@ retry:
 			     !(gfp_mask & __GFP_RETRY_MAYFAIL)))
 		goto nopage;
 
+	/* Boost when memory is low so allocation latency doesn't get too bad */
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPU_DDR_BW, 100);
+
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
 		goto retry;
