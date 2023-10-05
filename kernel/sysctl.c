70712b2b0e014285f7582af95deecaad0e21b2ea
hridaya <info.hridayaprajapati@gmail.com>
2023-10-05 22:49:24 +0545

    sched/fair: Fix compilation issues for !CONFIG_SCHED_WALT
    Co-authored-by: Jimmy Shiu <jimmyshiu@google.com>
    Signed-off-by: hridaya <info.hridayaprajapati@gmail.com>
    Change-Id: I439a8bfa0dd1aa0d9debd29a67668f1818c03f05
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -636,7 +636,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
 	},
-#endif
 	{
 		.procname	= "sched_force_lb_enable",
 		.data		= &sysctl_sched_force_lb_enable,
@@ -646,6 +645,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname       = "sched_cstate_aware",
