45a99d7b088a2e5f96908b368e31ee3f468bc1ae
Kneba <abenkenary3@gmail.com>
2024-10-13 14:24:57 +0000

    Revert "f2fs: fix to do sanity check on inode type during garbage collection"
    This reverts commit 09f19254924f60afeb78df7dbf995a81b5c8ccf8.

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1020,8 +1020,7 @@ next_step:
 
 		if (phase == 3) {
 			inode = f2fs_iget(sb, dni.ino);
-			if (IS_ERR(inode) || is_bad_inode(inode) ||
-					special_file(inode->i_mode))
+			if (IS_ERR(inode) || is_bad_inode(inode))
 				continue;
 
 			if (!down_write_trylock(
