2d672464c36158970674e78f092e16dc150c9f68
Meisie Widuri <ewprjkt@proton.me>
2024-11-12 09:42:52 +0700

    fs: namespace: Don't mentioned macro is defined

--- fs/namespace.c
+++ fs/namespace.c
@@ -1649,7 +1649,6 @@ static inline bool may_mandlock(void)
 }
 #endif
 
-#ifdef CONFIG_KSU
 static int can_umount(const struct path *path, int flags)
 {
 	struct mount *mnt = real_mount(path->mnt);
@@ -1683,7 +1682,6 @@ int path_umount(struct path *path, int flags)
 	mntput_no_expire(mnt);
 	return ret;
 }
-#endif
 
 /*
  * Now umount can handle mount points as well as block devices.
