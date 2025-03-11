a73a0780f8ee31bdb81c663b29f55a3c543f836d
Agus Suristyono <sandatjepil@proton.me>
2025-03-11 12:02:35 +0700

    block: switch bad_sector printk to pr_debug
    spam
    
    Change-Id: I45d201e42ef2c9913f97f0567d62ab456eee3591

--- block/blk-core.c
+++ block/blk-core.c
@@ -2124,8 +2124,8 @@ static void handle_bad_sector(struct bio *bio, sector_t maxsector)
 {
 	char b[BDEVNAME_SIZE];
 
-	printk(KERN_INFO "attempt to access beyond end of device\n");
-	printk(KERN_INFO "%s: rw=%d, want=%Lu, limit=%Lu\n",
+	pr_debug(KERN_INFO "attempt to access beyond end of device\n");
+	pr_debug(KERN_INFO "%s: rw=%d, want=%Lu, limit=%Lu\n",
 			bio_devname(bio, b), bio->bi_opf,
 			(unsigned long long)bio_end_sector(bio),
 			(long long)maxsector);
