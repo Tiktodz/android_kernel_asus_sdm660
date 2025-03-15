88ac49a195a71937e5cd7f9dd7e190cbed8a549b
Kneba <abenkenary3@gmail.com>
2025-03-15 20:11:15 +0000

    Make it OverClock by default
    gayming user happy!
    
    Change-Id: I6f6cf8d3ffbc0e5226c3ee80a5ee3c6fef1a93f8

--- drivers/clk/qcom/clk-cpu-osm-660.c
+++ drivers/clk/qcom/clk-cpu-osm-660.c
@@ -260,7 +260,7 @@ enum clk_osm_trace_packet_id {
 #define BM(msb, lsb)	(((((uint32_t)-1) << (31-msb)) >> (31-msb+lsb)) << lsb)
 #define BVAL(msb, lsb, val)	(((val) << lsb) & BM(msb, lsb))
 
-unsigned int is_cpu_overclocked = 0;
+unsigned int is_cpu_overclocked = 1;
 
 static int __init read_cpu_overclock_state(char *s)
 {
