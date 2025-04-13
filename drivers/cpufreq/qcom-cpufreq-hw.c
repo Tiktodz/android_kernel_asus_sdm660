628167762120ed33caa40967634a8627331f3734
Kneba <abenkenary3@gmail.com>
2025-04-13 05:45:26 +0000

    Revert "cpufreq: qcom-hw: Don't register frequency to OPP table if it is skipped"
    This reverts commit f7a257735e97026650964155d19dad90aacad85e.

--- drivers/cpufreq/qcom-cpufreq-hw.c
+++ drivers/cpufreq/qcom-cpufreq-hw.c
@@ -497,7 +497,6 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 	int ret, of_len;
 	u32 *of_table = NULL;
 	char tbl_name[] = "qcom,cpufreq-table-##";
-	bool invalidate_freq;
 
 	c->table = devm_kcalloc(dev, lut_max_entries + 1,
 				sizeof(*c->table), GFP_KERNEL);
@@ -551,7 +550,6 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 		if (!of_find_freq(of_table, of_len, c->table[i].frequency)) {
 			c->table[i].frequency = CPUFREQ_ENTRY_INVALID;
 			cur_freq = CPUFREQ_ENTRY_INVALID;
-			invalidate_freq = true;
 		} else {
 			if (core_count != c->max_cores) {
 				if (core_count == (c->max_cores - 1)) {
@@ -585,7 +583,6 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 				}
 				break;
 			}
-			invalidate_freq = false;
 		}
 
 		prev_cc = core_count;
@@ -595,9 +592,8 @@ static int qcom_cpufreq_hw_read_lut(struct platform_device *pdev,
 			cpu_dev = get_cpu_device(cpu);
 			if (!cpu_dev)
 				continue;
-			if (!invalidate_freq)
-				dev_pm_opp_add(cpu_dev, c->table[i].frequency * 1000,
-								volt);
+			dev_pm_opp_add(cpu_dev, c->table[i].frequency * 1000,
+							volt);
 		}
 	}
 
