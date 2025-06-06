b3af1af007bfb5052eb32bdf8fa7662ccdc251cb
rsuntk <rissu.ntk@gmail.com>
2025-06-06 02:43:33 +0000

    fs: sdfat: fix implicit declaration for 'sdfat_debug_warn_on'
    Fix error when SDFAT_DEBUG not defined
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- fs/sdfat/sdfat.h
+++ fs/sdfat/sdfat.h
@@ -448,6 +448,7 @@ void sdfat_debug_check_clusters(struct inode *inode);
 
 #define sdfat_debug_check_clusters(inode)
 #define sdfat_debug_bug_on(expr)
+#define sdfat_debug_warn_on(expr)
 
 #endif /* CONFIG_SDFAT_DEBUG */
 
