72090a1bf4f3c7e13d12b2f44f5bf047a9d620d2
strongreasons <abenkenari@gmail.com>
2025-06-21 04:48:18 +0000

    [SQUASH] "Revert: Introduce SBalance IRQ balancer"
    This reverts commit ba82ff6619d0d68c78b9839b86edeada0713231f.
    This reverts commit 05e459e3876b6e98777f8965c2c330c3cc100798.
    This reverts commit ed1ef6ff953e363e86e7ec6679278c905e333d3a.
    This reverts commit e317a683d575b1b51f9cddd7e2aaf4a0bc4bbf51.
    This reverts commit 81d01e179c41c3703b66058c41eb6dc40efb0473.
    This reverts commit a95e2d8c55887feac9736357eaa774ba870c57e9.
    This reverts commit 92f5a89cb9f5712e1facbdeefe994e5c2a4f5d3c.
    This reverts commit 9878e4687ba75803e96bf6e34212815724b29d26.
    This reverts commit 76b9f2ec25097bc01c1a6782ddfd828ecce7dac9.
    This reverts commit c5a2b24ed09d8619a40ea5cb1be91854687414a4.
    This reverts commit d2a6ad32dd6cc88c5a4daf13debafec780044867.
    This reverts commit a6c2ae93e537b7c12bfc30a7beb083863b0661a2.
    This reverts commit 9c12b08ede99c8a082bbcefdb3d800792f16a1bc.
    This reverts commit 15bb6a62b99f9fd0525c8e35330c63fa5f6ba3d2.
    
    Change-Id: If2bec9faebfc2d634fc331667221c27db606d259

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -155,7 +155,7 @@ EXPORT_SYMBOL(synchronize_irq);
 #ifdef CONFIG_SMP
 cpumask_var_t irq_default_affinity;
 
-bool __irq_can_set_affinity(struct irq_desc *desc)
+static bool __irq_can_set_affinity(struct irq_desc *desc)
 {
 	if (!desc || !irqd_can_balance(&desc->irq_data) ||
 	    !desc->irq_data.chip || !desc->irq_data.chip->irq_set_affinity)
@@ -240,8 +240,6 @@ int irq_do_set_affinity(struct irq_data *data, const struct cpumask *mask,
 	if (!chip || !chip->irq_set_affinity)
 		return -EINVAL;
 
-	/* IRQs only run on the first CPU in the affinity mask; reflect that */
-	mask = cpumask_of(cpumask_first(mask));
 	ret = chip->irq_set_affinity(data, mask, force);
 	switch (ret) {
 	case IRQ_SET_MASK_OK:
