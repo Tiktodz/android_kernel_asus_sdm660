d51815278da7483f58e72f5a2e6072435c8a8c11
Sergey Bashirov <sergeybashirov@gmail.com>
2025-10-03 12:11:03 +0300

    NFSD/blocklayout: Fix minlength check in proc_layoutget
    [ Upstream commit 3524b021b0ec620a76c89aee78e9d4b4130fb711 ]
    
    The extent returned by the file system may have a smaller offset than
    the segment offset requested by the client. In this case, the minimum
    segment length must be checked against the requested range. Otherwise,
    the client may not be able to continue the read/write operation.
    
    Fixes: 8650b8a05850 ("nfsd: pNFS block layout driver")
    Signed-off-by: Sergey Bashirov <sergeybashirov@gmail.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- fs/nfsd/blocklayout.c
+++ fs/nfsd/blocklayout.c
@@ -25,6 +25,7 @@ nfsd4_block_proc_layoutget(struct inode *inode, const struct svc_fh *fhp,
 {
 	struct nfsd4_layout_seg *seg = &args->lg_seg;
 	struct super_block *sb = inode->i_sb;
+	u64 length;
 	u32 block_size = i_blocksize(inode);
 	struct pnfs_block_extent *bex;
 	struct iomap iomap;
@@ -55,7 +56,8 @@ nfsd4_block_proc_layoutget(struct inode *inode, const struct svc_fh *fhp,
 		goto out_error;
 	}
 
-	if (iomap.length < args->lg_minlength) {
+	length = iomap.offset + iomap.length - seg->offset;
+	if (length < args->lg_minlength) {
 		dprintk("pnfsd: extent smaller than minlength\n");
 		goto out_layoutunavailable;
 	}
