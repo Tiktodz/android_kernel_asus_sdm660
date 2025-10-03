59a88e4937c49cc3964196882b5ae0498829f195
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2025-10-03 16:55:36 -0400

    media: imon: grab lock earlier in imon_ir_change_protocol()
    [ Upstream commit 7019553ab850ce1d3f0e512e16d14ab153f91c04 ]
    
    Move mutex_trylock() in imon_ir_change_protocol() to the beginning,
    for memcpy() which modifies ictx->usb_tx_buf should be protected by
    ictx->lock.
    
    Also, verify at the beginning of send_packet() that ictx->lock is held
    in case send_packet() is by error called from imon_ir_change_protocol()
    when mutex_trylock() failed due to concurrent requests.
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>
    Stable-dep-of: fa0f61cc1d82 ("media: rc: fix races with imon_disconnect()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- drivers/media/rc/imon.c
+++ drivers/media/rc/imon.c
@@ -607,6 +607,8 @@ static int send_packet(struct imon_context *ictx)
 	int retval = 0;
 	struct usb_ctrlrequest *control_req = NULL;
 
+	lockdep_assert_held(&ictx->lock);
+
 	/* Check if we need to use control or interrupt urb */
 	if (!ictx->tx_control) {
 		pipe = usb_sndintpipe(ictx->usbdev_intf0,
@@ -1135,7 +1137,7 @@ static int imon_ir_change_protocol(struct rc_dev *rc, u64 *rc_proto)
 	int retval;
 	struct imon_context *ictx = rc->priv;
 	struct device *dev = ictx->dev;
-	bool unlock = false;
+	const bool unlock = mutex_trylock(&ictx->lock);
 	unsigned char ir_proto_packet[] = {
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x86 };
 
@@ -1161,8 +1163,6 @@ static int imon_ir_change_protocol(struct rc_dev *rc, u64 *rc_proto)
 	}
 
 	memcpy(ictx->usb_tx_buf, &ir_proto_packet, sizeof(ir_proto_packet));
-
-	unlock = mutex_trylock(&ictx->lock);
 
 	retval = send_packet(ictx);
 	if (retval)
