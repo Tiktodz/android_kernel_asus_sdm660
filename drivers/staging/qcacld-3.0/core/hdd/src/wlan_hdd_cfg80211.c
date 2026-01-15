a8032e5f9b4e3d12695daf8a9b27258c041f239c
SonicBSV <sonicbsv@ya.ru>
2026-01-15 04:58:48 +0300

    drivers: staging: qcacld-3.0: fix kernel panic

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -23343,9 +23343,11 @@ static void hdd_update_chan_info(struct hdd_context *hdd_ctx,
 			struct scan_chan_info *chan,
 			struct scan_chan_info *info, uint32_t cmd_flag)
 {
+	/* Only process START_RESP and END_RESP, silently skip others */
 	if ((info->cmd_flag != WMI_CHAN_InFO_START_RESP) &&
-	   (info->cmd_flag != WMI_CHAN_InFO_END_RESP))
-		hdd_err("cmd flag is invalid: %d", info->cmd_flag);
+	    (info->cmd_flag != WMI_CHAN_InFO_END_RESP)) {
+		return;
+	}
 
 	mutex_lock(&hdd_ctx->chan_info_lock);
 
